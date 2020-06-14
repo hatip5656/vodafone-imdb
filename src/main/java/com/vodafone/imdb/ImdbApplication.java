303438337e2103f8b895ee8c1360f27b1ffbe90b
Hatip Aksünger <hatip.aksunger@bulutmd.com>
2020-06-14 11:12:40 +0300

    Revert "test data implemented"
    This reverts commit 6a95314e

--- src/main/java/com/vodafone/imdb/ImdbApplication.java
+++ src/main/java/com/vodafone/imdb/ImdbApplication.java
@@ -1,19 +1,13 @@
 package com.vodafone.imdb;
 
-import org.mapstruct.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -31,14 +25,5 @@ public class ImdbApplication {
 				.paths(PathSelectors.any())
 				.build();
 
-	}
-	@Bean
-	public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
-		ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-		resourceDatabasePopulator.addScript(new ClassPathResource("/data.sql"));
-		DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-		dataSourceInitializer.setDataSource(dataSource);
-		dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-		return dataSourceInitializer;
 	}
 }
